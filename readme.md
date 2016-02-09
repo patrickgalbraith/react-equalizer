884355b279e9d7d977916bf5c5e9e2ebf65884e4
Patrick <patrick.g@thedigitalembassy.co>
2016-02-10 09:43:16 +1030

    Initial commit

---
+++ readme.md
@@ -0,0 +1,21 @@
+# React Equalizer
+
+Pure React component which equalizes heights of child components.
+
+### Usage
+
+```
+<Equalizer>
+  <div>Child 1</div>
+  <div>Child 2</div>
+  <div>Child 3</div>
+</Equalizer>
+```
+
+### Usage
+* Add support for rows
+* Add support for responsive breakpoints
+
+### References
+* Zurb Foundation Equalizer
+* jQuery Match Height
---
+++ src/equalizer.js
@@ -0,0 +1,62 @@
+import React, { Component, PropTypes } from 'react'
+import ReactDOM from 'react-dom'
+
+export default class Equalizer extends Component {
+  constructor(){
+    super()
+    this.handleResize = this.handleResize.bind(this)
+  }
+
+  componentDidMount() {
+    this.handleResize()
+    window.addEventListener('resize', this.handleResize)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize)
+  }
+
+  componentDidUpdate() {
+    this.handleResize()
+  }
+
+  handleResize() {
+    window.setTimeout(this.updateChildrenHeights(), 0)
+  }
+
+  updateChildrenHeights() {
+    const node = ReactDOM.findDOMNode(this)
+    let maxHeight = 0
+
+    if (node !== undefined) {
+      for (let i=0; i < node.children.length; i++) {
+        let childEl = node.children[i]
+
+        childEl.style.height = ''
+
+        let height = childEl.offsetHeight
+
+        if(height > maxHeight) {
+          maxHeight = height
+        }
+      }
+
+      for (let i=0; i < node.children.length; i++) {
+        let childEl = node.children[i]
+        childEl.style.height = maxHeight
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
+Equalizer.propTypes = {
+  children: React.PropTypes.node.isRequired
+}
